0f86daca1e4baa8a872c5160625b38ebfe5c6406
beboopr <linnets-router.09@icloud.com>
2022-12-22 14:50:45 -0500

    setting up all backend routes and security middle ware with Auth from firestore

---
+++ server/controllers/auth.js
@@ -0,0 +1,63 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+//Register User
+/* Were going to encrypt the password, were going to
+save it and then when user tries to log in, there goona
+provide the password, were going to take that password
+solve it again and were going to make sure thats the correct
+one and give them a Json-web-token
+*/
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//LOGGIN IN
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ error: err.message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
